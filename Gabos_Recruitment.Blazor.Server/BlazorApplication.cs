3dc2012f76dd7fa7efa4c633d8386b80067316d6
Patryk Wyprzał <pwyprzal@gabos.pl>
2021-10-13 23:14:52 +0200

    Update docker-compose

--- Gabos_Recruitment.Blazor.Server/BlazorApplication.cs
+++ Gabos_Recruitment.Blazor.Server/BlazorApplication.cs
@@ -7,15 +7,20 @@ using Microsoft.Extensions.DependencyInjection;
 using DevExpress.ExpressApp.Xpo;
 using Gabos_Recruitment.Blazor.Server.Services;
 
-namespace Gabos_Recruitment.Blazor.Server {
-    public partial class Gabos_RecruitmentBlazorApplication : BlazorApplication {
-        public Gabos_RecruitmentBlazorApplication() {
+namespace Gabos_Recruitment.Blazor.Server
+{
+    public partial class Gabos_RecruitmentBlazorApplication : BlazorApplication
+    {
+        public Gabos_RecruitmentBlazorApplication()
+        {
             InitializeComponent();
         }
-        protected override void OnSetupStarted() {
+        protected override void OnSetupStarted()
+        {
             base.OnSetupStarted();
             IConfiguration configuration = ServiceProvider.GetRequiredService<IConfiguration>();
-            if(configuration.GetConnectionString("ConnectionString") != null) {
+            if (configuration.GetConnectionString("ConnectionString") != null)
+            {
                 ConnectionString = configuration.GetConnectionString("ConnectionString");
             }
 #if EASYTEST
@@ -24,46 +29,38 @@ namespace Gabos_Recruitment.Blazor.Server {
             }
 #endif
 #if DEBUG
-            if(System.Diagnostics.Debugger.IsAttached && CheckCompatibilityType == CheckCompatibilityType.DatabaseSchema) {
-                DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
-            }
+            DatabaseUpdateMode = DatabaseUpdateMode.UpdateDatabaseAlways;
 #endif
         }
-        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args) {
+
+        protected override void CreateDefaultObjectSpaceProvider(CreateCustomObjectSpaceProviderEventArgs args)
+        {
             IXpoDataStoreProvider dataStoreProvider = GetDataStoreProvider(args.ConnectionString, args.Connection);
             args.ObjectSpaceProviders.Add(new XPObjectSpaceProvider(dataStoreProvider, true));
             args.ObjectSpaceProviders.Add(new NonPersistentObjectSpaceProvider(TypesInfo, null));
         }
-        private IXpoDataStoreProvider GetDataStoreProvider(string connectionString, System.Data.IDbConnection connection) {
+
+        private IXpoDataStoreProvider GetDataStoreProvider(string connectionString, System.Data.IDbConnection connection)
+        {
             XpoDataStoreProviderAccessor accessor = ServiceProvider.GetRequiredService<XpoDataStoreProviderAccessor>();
-            lock(accessor) {
-                if(accessor.DataStoreProvider == null) {
+            lock (accessor)
+            {
+                if (accessor.DataStoreProvider == null)
+                {
                     accessor.DataStoreProvider = XPObjectSpaceProvider.GetDataStoreProvider(connectionString, connection, true);
                 }
             }
             return accessor.DataStoreProvider;
         }
-        private void Gabos_RecruitmentBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e) {
+
+        private void Gabos_RecruitmentBlazorApplication_DatabaseVersionMismatch(object sender, DatabaseVersionMismatchEventArgs e)
+        {
 #if EASYTEST
             e.Updater.Update();
             e.Handled = true;
 #else
-            if(System.Diagnostics.Debugger.IsAttached) {
-                e.Updater.Update();
-                e.Handled = true;
-            }
-            else {
-                string message = "The application cannot connect to the specified database, " +
-                    "because the database doesn't exist, its version is older " +
-                    "than that of the application or its schema does not match " +
-                    "the ORM data model structure. To avoid this error, use one " +
-                    "of the solutions from the https://www.devexpress.com/kb=T367835 KB Article.";
-
-                if(e.CompatibilityError != null && e.CompatibilityError.Exception != null) {
-                    message += "\r\n\r\nInner exception: " + e.CompatibilityError.Exception.Message;
-                }
-                throw new InvalidOperationException(message);
-            }
+            e.Updater.Update();
+            e.Handled = true;
 #endif
         }
     }
